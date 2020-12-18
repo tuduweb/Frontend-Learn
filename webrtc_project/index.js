f69701e451a1f21ddf3d2262cd97b3b08a1db208
tuduweb <tuduweb@qq.com>
2020-12-18 23:08:28 +0800

    多用户 同步用户列表

--- webrtc_project/index.js
+++ webrtc_project/index.js
@@ -1,4 +1,5 @@
-var app = require('express')();
+var express = require('express');
+var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
@@ -6,16 +7,38 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
+//还需要确定一下路由的方式
+app.get('/record', (req, res) => {
+    res.sendFile(__dirname + '/record.html');
+})
+
+
+app.use("/static", express.static('static/'));
+
+
+
 io.on('connection', (socket) => {
+    socket.join(socket.id);
+    //当建立连接时，可以选择服务器自动发送当前存在的用户出来
     console.log('a user connected : ' + socket.id);
     socket.on('disconnect', () => {
         console.log('user disconnected : ' + socket.id);
+        socket.broadcast.emit('user disconnected', socket.id);
     });
 
     socket.on('chat message', (msg) => {
         console.log(socket.id + ' say: ' + msg);
         socket.broadcast.emit('chat message', msg);
     });
+
+    socket.on('new user greet', (data) => {
+        console.log(socket.id + ' greet ' + data.msg);
+        socket.broadcast.emit('need connect', {sender: socket.id, msg : data.msg});
+    })
+
+    socket.on('ok we connect', (data) => {
+        io.to(data.receiver).emit('ok we connect', {sender : data.sender});
+    })
 
 });
 
