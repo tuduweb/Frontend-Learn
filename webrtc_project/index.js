95430cfb96872d8aebcbba880ac69eb406bb03a8
bin <tuduweb@qq.com>
2020-12-25 12:22:45 +0800

    更新到教程文档（三）版本。

--- webrtc_project/index.js
+++ webrtc_project/index.js
@@ -1,24 +1,40 @@
 var app = require('express')();
 var http = require('http').createServer(app);
+
+var fs = require('fs');
+let sslOptions = {
+    key: fs.readFileSync('C:/privkey.key'),//里面的文件替换成你生成的私钥
+    cert: fs.readFileSync('C:/cacert.pem')//里面的文件替换成你生成的证书
+};
+
+const https = require('https').createServer(sslOptions, app);
+
 var io = require('socket.io')(http);
+
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
   });
 
-io.on('connection', (socket) => {
-    console.log('a user connected : ' + socket.id);
-    socket.on('disconnect', () => {
-        console.log('user disconnected : ' + socket.id);
-    });
-
-    socket.on('chat message', (msg) => {
-        console.log(socket.id + ' say: ' + msg);
-        socket.broadcast.emit('chat message', msg);
-    });
-
+app.get('/camera', (req, res) => {
+    res.sendFile(__dirname + '/camera.html');
 });
 
-http.listen(3000, () => {
-    console.log('listening on *:3000');
-  });
+io.on("connection", (socket) => {
+    console.log("a user connected " + socket.id);
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected: " + socket.id);
+    })
+
+    socket.on("chat message",(msg) => {
+        console.log(socket.id + " say: " + msg);
+        //io.emit("chat message", msg);
+        socket.broadcast.emit("chat message", msg);
+    })
+})
+
+
+https.listen(4443, () => {
+    console.log('https listening on *:4443');
+});
